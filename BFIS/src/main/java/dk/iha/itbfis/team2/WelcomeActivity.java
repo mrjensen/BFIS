e4665a5b37102576fbe83847ee55f2b2556925a2
Martin Jensen <martin2610@gmail.com>
2013-11-22 13:57:59 +0100

    WelcomeActivity created.

--- BFIS/src/main/java/dk/iha/itbfis/team2/WelcomeActivity.java
+++ BFIS/src/main/java/dk/iha/itbfis/team2/WelcomeActivity.java
@@ -1,19 +1,15 @@
 package dk.iha.itbfis.team2;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 /**
  * Created by mje on 22/11/13.
  */
-public class LoginActivity extends Activity {
+public class WelcomeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_welcome);
 
     }
 }
