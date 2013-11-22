15495f7be9c13aed390a86e63a27e4af1dbac409
Kasper Saaby <kdsaaby@gmail.com>
2013-11-22 14:04:21 +0100

    Arhh!

---
+++ BFIS/src/main/java/dk/iha/itbfis/team2/TodoActivity.java
@@ -0,0 +1,13 @@
+package dk.iha.itbfis.team2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kaspersaaby on 22/11/13.
+ */
+public class TodoActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
