7bc741f83326ec8d02d68c68b5a9c7ea2e23a05c
Martin Jensen <martin2610@gmail.com>
2013-11-22 14:36:03 +0100

    Login -> welcome created.

--- BFIS/src/main/java/dk/iha/itbfis/team2/LoginActivity.java
+++ BFIS/src/main/java/dk/iha/itbfis/team2/LoginActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by mje on 22/11/13.
@@ -14,6 +16,19 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        Button startButton = (Button) findViewById(R.id.btn_login);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = ((EditText) findViewById(R.id.et_username)).getText().toString();
+                String password = ((EditText) findViewById(R.id.et_password)).getText().toString();
+                if (username.equals("bente")  && password.equals("112")){
+                    Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
+
+
 }
