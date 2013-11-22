e92c38e04ef1508fb1cb5d5626df27a72388a0a5
Kasper Nissen <kaspernissen@gmail.com>
2013-11-22 13:57:08 +0100

    Bloodpressure initial commit

---
+++ BFIS/src/main/java/dk/iha/itbfis/team2/BloodPressureActivity.java
@@ -0,0 +1,14 @@
+package dk.iha.itbfis.team2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kaspernissen on 22/11/13.
+ */
+public class BloodPressureActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bloodpressure);
+    }
+}
