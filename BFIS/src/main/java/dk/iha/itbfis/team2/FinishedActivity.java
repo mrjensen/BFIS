f09d708872b0b49de3c389d6bf4610a1e10c5219
MrJensen <martin2610@gmail.com>
2013-11-29 15:46:13 +0100

    Finished activity added.

---
+++ BFIS/src/main/java/dk/iha/itbfis/team2/FinishedActivity.java
@@ -0,0 +1,14 @@
+package dk.iha.itbfis.team2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mje on 29/11/13.
+ */
+public class FinishedActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finished);
+    }
+}
--- BFIS/src/main/java/dk/iha/itbfis/team2/PatientActivity.java
+++ BFIS/src/main/java/dk/iha/itbfis/team2/PatientActivity.java
@@ -1,7 +1,9 @@
 package dk.iha.itbfis.team2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 /**
  * Created by kaspernissen on 29/11/13.
@@ -9,5 +11,19 @@ import android.os.Bundle;
 public class PatientActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patientinfo);
+
+        CountDownTimer countDownTimer = new CountDownTimer(5000,5000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(PatientActivity.this, TodoActivity.class);
+                startActivity(intent);
+            }
+        }.start();
     }
 }
