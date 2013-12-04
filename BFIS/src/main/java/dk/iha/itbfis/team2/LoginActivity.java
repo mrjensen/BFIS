7b0ae58b4ae8ef2df95a426ffa69c70e7be05d5a
Kasper Nissen <kaspernissen@gmail.com>
2013-12-04 10:02:46 +0100

    Login failed handling

--- BFIS/src/main/java/dk/iha/itbfis/team2/LoginActivity.java
+++ BFIS/src/main/java/dk/iha/itbfis/team2/LoginActivity.java
@@ -1,12 +1,14 @@
 package dk.iha.itbfis.team2;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by mje on 22/11/13.
@@ -16,16 +18,22 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Context context = this;
+
         Button startButton = (Button) findViewById(R.id.btn_login);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String username = ((EditText) findViewById(R.id.et_username)).getText().toString();
                 String password = ((EditText) findViewById(R.id.et_password)).getText().toString();
-                if (username.equals("bente")  && password.equals("112")){
+                if (username.toLowerCase().equals("bente")  && password.equals("112")){
                     Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
                     startActivity(intent);
                 }
+                else{
+                    Toast.makeText(getApplicationContext(), "Brugernavn og password matcher ikke.", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
